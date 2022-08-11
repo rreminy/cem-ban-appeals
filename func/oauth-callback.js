dfa04dcf5fef320f1bbb8933ec729d47e5b7d194
Reminy Light <rreminy@gmail.com>
2022-08-11 18:12:30 -0400

    nope

--- func/oauth-callback.js
+++ func/oauth-callback.js
@@ -1,4 +1,4 @@
-//import fetch from "node-fetch";
+import fetch from "node-fetch";
 
 import { getUserInfo, getBan } from "./helpers/user-helpers.js";
 import { createJwt } from "./helpers/jwt-helpers.js";
