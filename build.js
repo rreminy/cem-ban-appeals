2bd6561ae84b2e4373b96ff2a613ae7a1319d7ab
Reminy Light <rreminy@gmail.com>
2022-08-11 11:27:56 -0400

    ES module

--- build.js
+++ build.js
@@ -1,55 +1,55 @@
-const Discord = require("discord.js");
-const fs = require("fs");
-const path = require("path");
-const process = require("process");
+import { Client } from "discord.js";
+import { rename, readFile, writeFile, unlink } from "fs";
+import { resolve } from "path";
+import { env, exit } from "process";
 
 async function main() {
-    if (!process.env.USE_NETLIFY_FORMS) {
-        const submissionOldFunction = path.resolve(__dirname, "func", "submission-created.js");
-        const submissionNewFunction = path.resolve(__dirname, "func", "submit-appeal.js");
-        fs.rename(submissionOldFunction, submissionNewFunction, err => {
+    if (!env.USE_NETLIFY_FORMS) {
+        const submissionOldFunction = resolve(__dirname, "func", "submission-created.js");
+        const submissionNewFunction = resolve(__dirname, "func", "submit-appeal.js");
+        rename(submissionOldFunction, submissionNewFunction, err => {
             if (err) {
                 console.log(err);
-                process.exit(1);
+                exit(1);
             }
         });
 
-        const form = path.resolve(__dirname, "public", "form.html");
-        fs.readFile(form, "UTF-8", (err, data) => {
+        const form = resolve(__dirname, "public", "form.html");
+        readFile(form, "UTF-8", (err, data) => {
             if (err) {
                 console.log(err);
-                process.exit(1);
+                exit(1);
             }
 
             data = data.replace("action=\"/success\" netlify", "action=\"/.netlify/functions/submit-appeal\"");
-            fs.writeFile(form, data, "UTF-8", err => {
+            writeFile(form, data, "UTF-8", err => {
                 if (err) {
                     console.log(err);
-                    process.exit(1);
+                    exit(1);
                 }
             });
         });
     }
 
-    if (process.env.DISABLE_UNBAN_LINK) {
-        const unban = path.resolve(__dirname, "func", "unban.js");
-        fs.unlink(unban, err => {
+    if (env.DISABLE_UNBAN_LINK) {
+        const unban = resolve(__dirname, "func", "unban.js");
+        unlink(unban, err => {
             if (err) {
                 console.log(err);
-                process.exit(1);
+                exit(1);
             }
         });
     }
 
     // Make sure the bot connected to the gateway at least once.
-    const client = new Discord.Client({
+    const client = new Client({
         intents: 0x1ffff
     });
     try {
-        await client.login(process.env.DISCORD_BOT_TOKEN);
+        await client.login(env.DISCORD_BOT_TOKEN);
     } catch (e) {
         console.log(e);
-        process.exit(1);
+        exit(1);
     }
     client.destroy();
 }
